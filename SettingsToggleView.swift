5babd7522688af914defb52e209ce005b0553623
RS <rheasreedhar25@gmail.com>
2025-07-29 11:03:30 -0700

    Create SettingsToggleView.swift

---
+++ SettingsToggleView.swift
@@ -0,0 +1,25 @@
+import SwiftUI
+
+struct SettingsToggle: View {
+    let title: String
+    let subtitle: String
+    @Binding var isOn: Bool
+    
+    var body: some View {
+        HStack {
+            VStack(alignment: .leading, spacing: 2) {
+                Text(title)
+                    .fontWeight(.medium)
+                
+                Text(subtitle)
+                    .font(.caption)
+                    .foregroundColor(.secondary)
+            }
+            
+            Spacer()
+            
+            Toggle("", isOn: $isOn)
+                .labelsHidden()
+        }
+    }
+}
