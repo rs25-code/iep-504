e06a90887cd4a9c1b7189fd1fb96eb7bbb00d18a
RS <rheasreedhar25@gmail.com>
2025-07-29 11:03:53 -0700

    Create StatCardView.swift

---
+++ StatCardView.swift
@@ -0,0 +1,27 @@
+import SwiftUI
+
+struct StatCard: View {
+    let title: String
+    let value: String
+    let icon: String
+    
+    var body: some View {
+        VStack(spacing: 8) {
+            Image(systemName: icon)
+                .font(.title2)
+                .foregroundColor(.blue)
+            
+            Text(value)
+                .font(.title2)
+                .fontWeight(.bold)
+            
+            Text(title)
+                .font(.caption)
+                .foregroundColor(.secondary)
+                .multilineTextAlignment(.center)
+        }
+        .padding()
+        .background(Color(.systemGray6))
+        .cornerRadius(12)
+    }
+}
