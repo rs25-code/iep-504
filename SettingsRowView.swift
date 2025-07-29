05c3d1f605b06cbfd5c3fbb81058dd32ae2974af
RS <rheasreedhar25@gmail.com>
2025-07-29 11:02:35 -0700

    Create SettingsRowView.swift

---
+++ SettingsRowView.swift
@@ -0,0 +1,23 @@
+import SwiftUI
+
+struct SettingsRow: View {
+    let title: String
+    let action: () -> Void
+    
+    var body: some View {
+        Button(action: action) {
+            HStack {
+                Text(title)
+                    .fontWeight(.medium)
+                
+                Spacer()
+                
+                Image(systemName: "chevron.right")
+                    .font(.caption)
+                    .foregroundColor(.secondary)
+            }
+            .padding(.vertical, 8)
+        }
+        .foregroundColor(.primary)
+    }
+}
